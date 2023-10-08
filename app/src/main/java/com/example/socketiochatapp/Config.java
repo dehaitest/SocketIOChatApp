41eca6d7cae361e28b9610257d1845830ff928e4
dehaitest <64582895+dehaitest@users.noreply.github.com>
2023-10-08 23:29:48 +1100

    Add files via upload

---
+++ app/src/main/java/com/example/socketiochatapp/Config.java
@@ -0,0 +1,8 @@
+package com.example.socketiochatapp;
+public class Config {
+    //public static final String SERVER_IP = "http://192.168.1.22";
+    public static final String SERVER_IP = "http://***";
+
+    public static final int SERVER_PORT = 5000;
+    //... other configurations
+}
